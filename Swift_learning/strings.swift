cbc0d79ecc5804952df4bfe2b17f3517c92e6c63
Daniil <danast_2@mail.ru>
2024-10-03 23:15:28 +0300

    23:15

--- Swift_learning/strings.swift
+++ Swift_learning/strings.swift
@@ -9,6 +9,52 @@ import Foundation
 
 func str(){
     let char: Character = "a"
+    let string: String = "DanAst____"
+    var emptyString = ""
+    var anotherEmptyString = String()
+    /**При попытке инициализировать пустой строковый литерал параметру типа Character Xcode сообщит об ошибке несоответствия типов, так как
+     пустая строка не является отдельным символом (рис. 2.5). Она является пустой
+     строкой.*/
+    let longString = 
+    """
+     Это многострочный
+     строковый литерал
+    """
+    let notEmptyString = String("Hello, DanAst")
+    
+    // константа типа Double
+    let someDoubleNumber = 74.22
+    // строка, созданная на основе константы типа Double
+    let someStringNumber = String(someDoubleNumber) // "74.22"
+    
+    // переменная типа String
+    let name = "Дракон"
+    // константа типа String c использованием интерполяции
+    let hello = "Привет, меня зовут \(name)!"
+    // интерполяция с использованием выражения
+    let meters: Double = 10
+    let text = "Моя длина \(meters * 3.28) фута"
+    // выведем значения на консоль
+    print(hello)
+    print(text)
+    
+    
+    // константа типа String
+    let firstText = "Мой вес "
+    // переменная типа Double
+    let weight = 12.4
+    // константа типа String
+    let secondText = " тонны"
+    // конкатенация строк при инициализации значения новой переменной
+    let resultText = firstText + String(weight) + secondText
+    print(resultText)
+    
+    let firstString = "Swift"
+    let secondString = "Objective-C"
+    let anotherString = "Swift"
+    firstString == secondString // false
+    firstString == anotherString // true
+    
     print(char)
 }
 
