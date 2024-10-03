928bce3a65d59ac08468c2df39ec1ec09c7cdd98
Daniil <danast_2@mail.ru>
2024-10-03 23:02:17 +0300

    23:02

---
+++ Swift_learning/strings.swift
@@ -0,0 +1,14 @@
+//
+//  strings.swift
+//  Swift_learning
+//
+//  Created by Даниил Асташов on 03.10.2024.
+//
+
+import Foundation
+
+func str(){
+    let char: Character = "a"
+    print(char)
+}
+
