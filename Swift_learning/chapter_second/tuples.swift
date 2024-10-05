b392052a0ec5d5f6acd2667744f5546680ae4bfa
Daniil <danast_2@mail.ru>
2024-10-05 16:55:59 +0300

    16:55

--- Swift_learning/chapter_second/tuples.swift
+++ Swift_learning/chapter_second/tuples.swift
@@ -35,7 +35,36 @@ func getPerson() -> (String, Int){
 let firstPerson = (name: "Dan", age: 18)
 let secondPerson = (name: "Den", age: 22)
 
+//Сравнение кортежей: Кортежи можно сравнивать с помощью операторов сравнения, если их элементы также поддерживают сравнение
 func comperhension(fp: (name: String, age: Int), sp:(name: String, age: Int)) -> (Bool, Bool, Bool){
     //let ans = (fp_maj: fp.age > sp.age, sp_maj: fp.age < sp.age,eq: fp.age == sp.age)
     return (fp_maj: fp.age > sp.age, sp_maj: fp.age < sp.age,eq: fp.age == sp.age)
 }
+
+//можно записать значения кортежа в переменные
+let floatStatus: (Float, String, Bool) = (200.2, "In Work", true)
+//floatStatus // (.0 200.2, .1 "In Work", .2 true)
+// записываем значения кортежа в переменные
+let (statusCode, statusText, statusConnect) = floatStatus
+// выводим информацию
+//print("Код ответа — \(statusCode)")
+//print("Текст ответа — \(statusText)")
+//print("Связь с сервером — \(statusConnect)")
+
+/* объявляем две переменные с одновременной
+инициализацией им значений */
+var (myName1, myAge1) = ("Тролль", 140)
+// выводим их значения
+//print("Мое имя \(myName), и мне \(myAge) лет")
+
+/* получаем только необходимые
+значения кортежа */
+let (statusCodeNew, _, _) = floatStatus
+
+/* объявляем кортеж с
+указанием имен элементов
+в описании типа */
+let anotherStatusTuple: (statusCode: Int, statusText: String, statusConnect: Bool)
+= (200, "In Work", true)
+// выводим значение элемента
+//anotherStatusTuple.statusCode // 200
