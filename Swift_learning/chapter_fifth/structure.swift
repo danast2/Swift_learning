7783f4923f30077e16380a7be181c20e5c235faf
Daniil <danast_2@mail.ru>
2024-10-23 07:22:34 +0300

    structure_1

---
+++ Swift_learning/chapter_fifth/structure.swift
@@ -0,0 +1,16 @@
+//
+//  structure.swift
+//  Swift_learning
+//
+//  Created by Даниил Асташов on 23.10.2024.
+//
+
+import Foundation
+
+struct ChessPlayer {}
+
+let playerOleg = ChessPlayer()
+//type(of: playerOleg) // ChessPlayer.Type
+
+//свойства в структурах
+
